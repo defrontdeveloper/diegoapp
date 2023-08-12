96f028394daa6bb30fa9be9398f07ec17edcf7db
David <defrontdeveloper@gmail.com>
2023-08-12 09:25:17 -0500

    subida

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://defrontdeveloper.github.io/diegoapp'
+  base: 'https://defrontdeveloper.github.io/diegoapp/'
 })
